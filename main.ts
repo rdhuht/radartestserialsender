4288026a346822ad0a5eb86a25d40b7434975a11
JeremyTsui <44908010+rdhuht@users.noreply.github.com>
2022-10-24 11:20:27 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 serial.redirect(
 SerialPin.USB_TX,
 SerialPin.USB_RX,
-BaudRate.BaudRate9600
+BaudRate.BaudRate115200
 )
 let list: number[] = []
 for (let index = 0; index <= 180; index++) {
@@ -10,10 +10,10 @@ for (let index = 0; index <= 180; index++) {
 basic.forever(function () {
     for (let index = 0; index <= 180; index++) {
         serial.writeLine("/" + "*" + list[index] + "," + randint(200, 300) + "*" + "/")
-        basic.pause(100)
+        basic.pause(50)
     }
     for (let index = 0; index <= 180; index++) {
         serial.writeLine("/" + "*" + list[180 - index] + "," + randint(200, 300) + "*" + "/")
-        basic.pause(100)
+        basic.pause(50)
     }
 })
