2ac8b61b43f17b738c755f8ae259d36ec01f8ab6
JeremyTsui <44908010+rdhuht@users.noreply.github.com>
2022-10-24 10:54:04 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,10 +10,10 @@ for (let index = 0; index <= 180; index++) {
 basic.forever(function () {
     for (let index = 0; index <= 180; index++) {
         serial.writeLine("/" + "*" + list[index] + "," + randint(200, 300) + "*" + "/")
-        basic.pause(500)
+        basic.pause(100)
     }
     for (let index = 0; index <= 180; index++) {
         serial.writeLine("/" + "*" + list[180 - index] + "," + randint(200, 300) + "*" + "/")
-        basic.pause(500)
+        basic.pause(100)
     }
 })
